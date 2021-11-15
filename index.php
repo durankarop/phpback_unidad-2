f3314bd5ac7247f02accc0bf6c6a5ae4698cce22
Duran <durankarop@gmail.com>
2021-11-14 21:44:41 -0300

    Agregando index.php para el tp

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+$nombre = "Diego Alberto";
+$apellido = "Durante";
+$edad = 33;
+$hobbies = "Videojuegos, Pádel, Series, Catan";
+$editorPreferido = "Visual Studio Code";
+$sistemaOperativo = "Windows 10";
+
+echo "<br>";
+echo "Nombre: $nombre"; 
+echo "<br>";
+echo "Apellido: $apellido";
+echo "<br>";
+echo "Edad: $edad";
+echo "<br>";
+echo "Hobbies: $hobbies";
+echo "<br>";
+echo "Editor Preferido: $editorPreferido";
+echo "<br>";
+echo "Sistema Operativo: $sistemaOperativo";
+echo "<br>";
+
+?>
+
